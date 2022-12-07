184ae26e9228b0f6e875a3b26cad1619dc89a6cc
Puneet <puneet@racloop.com>
2022-12-07 15:17:14 +0530

    Added Cors on method level

--- src/main/java/com/abdm/sharegateway/controller/HealthInformationProviderController.java
+++ src/main/java/com/abdm/sharegateway/controller/HealthInformationProviderController.java
@@ -17,12 +17,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/hip")
-@CrossOrigin
 public class HealthInformationProviderController {
 
     @Autowired
     private HealthInformationProviderService healthInformationProviderService;
 
+    @CrossOrigin
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity create(@Valid @RequestPart String healthInformationProviderCO,
                                  @Valid @RequestPart MultipartFile multipartFile) {
@@ -34,20 +34,24 @@ public class HealthInformationProviderController {
     }
 
     @GetMapping()
+    @CrossOrigin
     public ResponseEntity<List<HealthInformationProviderVO>> list() {
         return ResponseEntity.ok(healthInformationProviderService.getAll());
     }
 
+    @CrossOrigin
     @GetMapping("/{hipId}/counter/{counterId}")
     public ResponseEntity<HealthInformationProviderDetailedVO> get(@PathVariable String hipId, @PathVariable String counterId) {
         return ResponseEntity.ok(healthInformationProviderService.get(hipId, counterId));
     }
 
+    @CrossOrigin
     @DeleteMapping("/{hipId}/counter/{counterId}")
     public ResponseEntity<HealthInformationProvider> delete(@PathVariable String hipId, @PathVariable String counterId) {
         return ResponseEntity.ok(healthInformationProviderService.delete(hipId, counterId));
     }
 
+    @CrossOrigin
     @PutMapping("/{hipId}/counter/{counterId}")
     public ResponseEntity<HealthInformationProvider> update(@PathVariable String hipId, @PathVariable String counterId,
                                                             @Valid @RequestPart String healthInformationProviderCO,
