9e4e448730701a4498830013e59a49605f3ff3dc
puneet <puneet@racloop.com>
2023-01-23 10:21:59 +0530

    Added log in Ping API

--- src/main/java/com/abdm/sharegateway/controller/TestController.java
+++ src/main/java/com/abdm/sharegateway/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.abdm.sharegateway.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/scan-gateway")
+@Slf4j
 public class TestController {
 
     @GetMapping("/ping")
-    ResponseEntity<String> test() {
+    ResponseEntity<String> test(@RequestBody String object) {
+        log.info(object);
         return ResponseEntity.ok("Pong from Scan-gateway!");
     }
 }
